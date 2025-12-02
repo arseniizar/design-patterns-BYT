41e2b0b3c9da6bc507d7a086ec6213a8c501218c
arseniizar <arseniizarudniuk@gmail.com>
2025-12-02 23:29:57 +0100

    finished observer; factory and adapter patterns

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -1,13 +1,28 @@
-﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
-namespace DesignPattern.Adapter
+﻿namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
-        private readonly BillingSystem thirdPartyBillingSystem = new();
+        private readonly BillingSystem _thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> listEmployee = new List<Employee>();
+
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                Employee employee = new Employee(
+                    int.Parse(employeesArray[i, 0]),
+                    employeesArray[i, 1],
+                    employeesArray[i, 2],
+                    decimal.Parse(employeesArray[i, 3])
+                );
+                listEmployee.Add(employee);
+            }
+
+            Console.WriteLine("Adapter converted array of employees to a list of employees.");
+            Console.WriteLine("Then, the adapter called the ProcessSalary method of the third-party billing system.");
+
+            _thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
     }
 }
--- DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,17 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType databaseType)
+        {
+            switch (databaseType)
+            {
+                case DatabaseType.SqlServer:
+                    return new SqlServerDatabase();
+                case DatabaseType.Oracle:
+                    return new OracleDatabase();
+                default:
+                    throw new ArgumentException("Invalid database type specified.");
+            }
+        }
     }
 }
